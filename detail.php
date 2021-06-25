936062e21fa8e1a056772b34e7c4b1ec9678e986
Martin Levi <martinl@seincomp.com.ar>
2021-06-25 15:58:31 -0400

    Ruta
    Cambio de ruta

--- detail.php
+++ detail.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <?php 
 
-    $direccion="https://2b653a48434a.ngrok.io/";
+    $direccion="https://martin-levi-mp-commerce-nodejs.herokuapp.com/";
     require __DIR__ .  '/vendor/autoload.php';
     // Agrega credenciales
     MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398'); 
